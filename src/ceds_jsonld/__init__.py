85f51318d98cf7812c377fb202826df148cfe45b
Dan <lunddavidacd@gmail.com>
2026-03-24 12:54:35 -0400

    chore: bump version to 1.0.0 and update CHANGELOG/ROADMAP

--- src/ceds_jsonld/__init__.py
+++ src/ceds_jsonld/__init__.py
@@ -36,7 +36,7 @@ from ceds_jsonld.validator import (
 )
 from ceds_jsonld.wizard import MappingWizard
 
-__version__ = "0.12.1"
+__version__ = "1.0.0"
 __all__ = [
     "APIAdapter",
     "BigQueryAdapter",
