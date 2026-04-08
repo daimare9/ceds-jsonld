8e3a703e84dce4bec5f20f64a9390967b6e8426d
Dan <lunddavidacd@gmail.com>
2026-04-08 15:50:12 -0400

    chore: bump version to 1.1.0 for ParquetAdapter release

--- src/ceds_jsonld/__init__.py
+++ src/ceds_jsonld/__init__.py
@@ -37,7 +37,7 @@ from ceds_jsonld.validator import (
 )
 from ceds_jsonld.wizard import MappingWizard
 
-__version__ = "1.0.4"
+__version__ = "1.1.0"
 __all__ = [
     "APIAdapter",
     "BigQueryAdapter",
